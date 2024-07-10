927e6e74036fe1b7c4e19ad1b208dbe8dfb198e9
Ahmad <malikzada.seyar@gmail.com>
2024-07-10 08:15:15 -0700

    My first commit

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -3,5 +3,7 @@ package helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world ");
+
+        System.out.println("Something");
     }
 }
